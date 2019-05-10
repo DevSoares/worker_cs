8a492f70cad73032d0da21323c85d5a8588a843a
Luiz Soares <dev.soares@outlook.com>
2019-05-10 15:45:03 -0300

    Inicial and last commit

---
+++ Entities/Department.cs
@@ -0,0 +1,17 @@
+﻿namespace AppWorker.Entities
+{
+    class Department
+    {
+        public string Name { get; set; }
+
+        public Department()
+        {
+
+        }
+
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}
